ba5f531b333039fd09bad49fdaa31a046bfbc37f
Kripth <kripth04@gmail.com>
2019-11-14 20:56:35 +0100

    Allow custom value mapping and handle non-string values by default

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = (data, options = {}) => {
 	const separator = options.separator || ",";
+	const mapper = options.mapper || (value => value + "");
 	return data.map(line => line.map(value => {
+			value = mapper(value);
 			const contains = char => value.indexOf(char) !== -1;
 			if(contains("\"")) {
 				return `"${value.replace(/"/g, "\"\"")}"`;
